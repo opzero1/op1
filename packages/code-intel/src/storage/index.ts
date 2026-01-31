ddc7887533f31eb828296d2a29d426e59fe755f4
afif <afif.alauddin@reap.hk>
2026-01-31 18:41:52 +0800

    chore(code-intel): export new storage modules

--- packages/code-intel/src/storage/index.ts
+++ packages/code-intel/src/storage/index.ts
@@ -20,8 +20,18 @@ export type { KeywordStore, KeywordSearchResult } from "./keyword-store";
 export { createVectorStore } from "./vector-store";
 export type { VectorStore, VectorSearchResult } from "./vector-store";
 
-export { createPureVectorStore, createHybridVectorStore } from "./pure-vector-store";
-export type { PureVectorStore, HybridVectorStore } from "./pure-vector-store";
+export { createPureVectorStore, createHybridVectorStore, createGranularVectorStore } from "./pure-vector-store";
+export type { PureVectorStore, HybridVectorStore, GranularVectorStore } from "./pure-vector-store";
 
 export { createRepoMapStore } from "./repo-map-store";
 export type { RepoMapStore } from "./repo-map-store";
+
+export { createChunkStore } from "./chunk-store";
+export type { ChunkStore } from "./chunk-store";
+
+export { createFileContentStore } from "./file-content-store";
+export type { FileContentStore } from "./file-content-store";
+
+export { createContentFTSStore } from "./content-fts-store";
+export type { ContentFTSStore, FTSEntry, FTSSearchResult, FTSSearchOptions } from "./content-fts-store";
+
